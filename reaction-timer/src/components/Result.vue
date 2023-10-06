cf054a030130bc2a96cb6cbce07717cfa426fe4f
Rameshzende <jhande00@gmail.com>
2023-10-05 21:40:29 -0700

    first reaction timer app created

---
+++ reaction-timer/src/components/Result.vue
@@ -0,0 +1,25 @@
+<template>
+    <p> My Reaction time is : {{ score }} ms</p>
+    {{ rank }}
+</template>
+
+<script>
+  export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+
+    mounted() {
+        if(this.score < 250) {
+            this.rank = 'Your are Awesome at it'
+        } else if(this.score < 500) {
+            this.rank = 'Ohh, Very Goog ninja '
+        } else {
+            this.rank = 'Good Buddy'
+        }
+    }
+  }
+</script>
